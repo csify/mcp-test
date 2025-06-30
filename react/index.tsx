566e7ff7a4188ba9105155a964f48a882aac671d
csify <105548473+csify@users.noreply.github.com>
2025-06-30 12:49:18 +0000

    Ordner aufgeräumt: Nur noch reine Komponenten-Dateien in den button-Ordnern, keine Demos oder Fremddateien

--- react/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ButtonDemo from './ButtonDemo';
-
-const root = document.getElementById('root');
-if (root) {
-  createRoot(root).render(<ButtonDemo />);
-}
